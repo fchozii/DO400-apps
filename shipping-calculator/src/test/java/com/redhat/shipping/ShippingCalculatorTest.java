40f83c0e6de851c16cd75f76b6f7d381d7b0a92a
Student User <student@workstation.lab.example.com>
2023-08-29 05:31:56 -0400

    finish exercise

--- shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
+++ shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
@@ -7,4 +7,59 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ShippingCalculatorTest {
 
     // @todo: add tests
+    // @Test
+    // public void canCalcualteTheCostForARegion() throws RegionNotFoundException {
+    //     //given
+    //     ShippingCalculator calculator = new ShippingCalculator();
+
+    //     //when then
+    //     assertEquals(0, calculator.costForRegion("A Region"));
+    // }
+
+    @Test
+    public void onNARegionTheCostIs100() throws RegionNotFoundException {
+        //given
+        ShippingCalculator calculator = new ShippingCalculator();
+        
+        //when then
+        assertEquals(100, calculator.costForRegion("NA"));
+    }
+
+    @Test
+    public void onLATAMRegionTheCostIs200() throws RegionNotFoundException {
+        //given
+        ShippingCalculator calculator = new ShippingCalculator();
+        
+        //when then
+        assertEquals(200, calculator.costForRegion("LATAM"));
+    }
+
+    @Test
+    public void onEMEARegionTheCostIs300() throws RegionNotFoundException {
+        //given
+        ShippingCalculator calculator = new ShippingCalculator();
+        
+        //when then
+        assertEquals(300, calculator.costForRegion("EMEA"));
+    }
+
+    @Test
+    public void onAPACRegionTheCostIs400() throws RegionNotFoundException {
+        //given
+        ShippingCalculator calculator = new ShippingCalculator();
+        
+        //when then
+        assertEquals(400, calculator.costForRegion("APAC"));
+    }
+
+    @Test
+    public void onNonSupportedRegionARegionNotFoundExceptionIsRaised(){
+        ShippingCalculator calculator = new ShippingCalculator();
+
+        assertThrows(
+            RegionNotFoundException.class, 
+            () -> calculator.costForRegion("Unknown Region")
+        );
+    }
+    
 }
